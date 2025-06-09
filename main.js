da3d5ba14bfd203a4a8971b4d5c3aaebe368e000
jaronyoshi <jaron.yoshimura@mds.ac.nz>
2025-06-09 14:37:27 +1200

    added superbaseclient.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const SUPABASE_URL = 'https://uovppzmlwkqgpvznipzu.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvdnBwem1sd2txZ3B2em5pcHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0MjgzMDYsImV4cCI6MjA2NTAwNDMwNn0.MbahXodbWZ7jUziYJQtg8hLt3l-P0M7wKZQJC7-QR70';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+  document.getElementById('name-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+
+    const { data, error } = await supabase
+      .from('support_form')
+      .insert([{ name }]);
+
+    if (error) {
+      console.error('Error saving name:', error.message);
+    } else {
+      console.log('Saved:', data);
+      window.location.href = 'onboarding6.html';
+    }
+
+  });
---
+++ supabaseClient.js
@@ -0,0 +1,3 @@
+  const SUPABASE_URL = 'https://uovppzmlwkqgpvznipzu.supabase.co';
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvdnBwem1sd2txZ3B2em5pcHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0MjgzMDYsImV4cCI6MjA2NTAwNDMwNn0.MbahXodbWZ7jUziYJQtg8hLt3l-P0M7wKZQJC7-QR70';
+  const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
